5f143321ef58389ff0eaa464d227edd25ee4394f
samee <samee@DESKTOP-9O54JP3>
2019-01-22 06:19:54 +0530

    Initial commit

---
+++ spring-boot-recipe-project/src/main/java/sameer/springframework/SpringBootRecipeProjectApplication.java
@@ -0,0 +1,14 @@
+package sameer.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRecipeProjectApplication.class, args);
+	}
+
+}
+
